6d3eef2514fcefd7ba3782d80daafd868d5c2215
keyi liu <1458929605@qq.com>
2023-12-12 20:40:07 +0800

    TM

--- DDB-Project2023/DDB-Project2023/DDB-Project2021/SourceCode/transaction/TransactionManager.java
+++ DDB-Project2023/DDB-Project2023/DDB-Project2021/SourceCode/transaction/TransactionManager.java
@@ -2,20 +2,81 @@ package transaction;
 
 import java.rmi.*;
 
-/** 
+import javax.transaction.InvalidTransactionException;
+
+/**
  * Interface for the Transaction Manager of the Distributed Travel
  * Reservation System.
  * <p>
  * Unlike WorkflowController.java, you are supposed to make changes
  * to this file.
  */
-
+/**
+ * Start(),commit(),abort()由WC调用，分别用来启动、提交、中断一个事务。启动一个事务时，记录当前事务的信息，commit或abort成功时，删除当前的事务信息
+ * enlist()函数由RM来使用，用来通知TM哪个RM参与了哪个事务
+ * 
+ * Resource Manager Interface 被Transaction Manager调用的方法 Prepare/Commit/Abort()
+ * transaction Manager Interface 被Resource Manager调用的方法Enlist()
+ * TM要解决的问题:
+ * 当发生错误时适时合理地中止事务
+ * 事务恢复和回滚
+ * 在错误解决时恢复原本可以正确执行的操作/把事务已做的操作全部撤销
+ * 
+ * 
+ */
 public interface TransactionManager extends Remote {
 
-    public boolean dieNow()
-	throws RemoteException;
+    public boolean dieNow() throws RemoteException;
 
+    public void ping() throws RemoteException;
+
+    public void enlist(int xid, ResourceManager rm) throws RemoteException;
+
+    public void setDieTime(String time) throws RemoteException;
+
+    /**
+     * Start a new transaction, and return its transaction id.
+     *
+     * @return A unique transaction ID > 0. Return <=0 if server is not accepting
+     *         new transactions.
+     *
+     * @throws RemoteException on communications failure.
+     */
+    public int start() throws RemoteException;
+
+    /**
+     * Commit transaction.
+     *
+     * @param xid id of transaction to be committed.
+     * @return true on success, false on failure.
+     *
+     * @throws RemoteException             on communications failure.
+     * @throws TransactionAbortedException if transaction was aborted.
+     * @throws InvalidTransactionException if transaction id is invalid.
+     */
+    public boolean commit(int xid)
+            throws RemoteException,
+            TransactionAbortedException,
+            InvalidTransactionException;
+
+    /**
+     * Abort transaction.
+     *
+     * @param xid id of transaction to be aborted.
+     *
+     * @throws RemoteException             on communications failure.
+     * @throws InvalidTransactionException if transaction id is invalid.
+     */
+    public void abort(int xid)
+            throws RemoteException,
+            InvalidTransactionException;
 
     /** The RMI name a TransactionManager binds to. */
     public static final String RMIName = "TM";
+
+    /** The transaction's status */
+    public static final String START = "START";
+    public static final String PREPARED = "PREPARED";
+    public static final String COMMITTED = "COMMITTED";
+    public static final String ABORTED = "ABORTED";
 }
--- DDB-Project2023/DDB-Project2023/DDB-Project2021/SourceCode/transaction/TransactionManagerImpl.java
+++ DDB-Project2023/DDB-Project2023/DDB-Project2021/SourceCode/transaction/TransactionManagerImpl.java
@@ -1,47 +1,168 @@
 package transaction;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.rmi.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
-/** 
- * Transaction Manager for the Distributed Travel Reservation System.
- * 
- * Description: toy implementation of the TM
+/**
+ * * Transaction Manager for the Distributed Travel Reservation System.
+ *
+ * * Description: toy implementation of the TM
  */
 
 public class TransactionManagerImpl
-    extends java.rmi.server.UnicastRemoteObject
-    implements TransactionManager {
-    
-    public static void main(String args[]) {
-	System.setSecurityManager(new RMISecurityManager());
+		extends java.rmi.server.UnicastRemoteObject
+		implements TransactionManager {
 
-	String rmiPort = System.getProperty("rmiPort");
-	if (rmiPort == null) {
-	    rmiPort = "";
-	} else if (!rmiPort.equals("")) {
-	    rmiPort = "//:" + rmiPort + "/";
+	// xid and status log data path
+	private final static String DATA_ROOT = "data/";
+	private final static String TM_COUNTER_LOG = DATA_ROOT + "tm_xidCounter.log";
+	private final static String TM_LOG = DATA_ROOT + "tm_xids.log";
+	private final static String TM_RMs_LOG = DATA_ROOT + "tm_xidRMs.log";
+	private final static String TM_RECOVER_LOG = DATA_ROOT + "tm_xidRecover.log";
+
+	private Integer xidCounter;// transaction id
+	private String dieTime;
+
+	// RMs(HashSet<ResourceManager>) of transaction(Integer xid)
+	private HashMap<Integer, HashSet<ResourceManager>> RMs;
+
+	// status(String) of transactions(Integer xid)
+	private HashMap<Integer, String> xids;
+
+	// transactions  to be recovered (Integer xid)) of transactions(Integer xid)
+	private HashMap<Integer, Integer> xids_to_be_recovered = new HashMap<>();
+
+	public static void main(String args[]) {
+		System.setSecurityManager(new RMISecurityManager());
+
+		String rmiPort = System.getProperty("rmiPort");
+		if (rmiPort == null) {
+			rmiPort = "";
+		} else if (!rmiPort.equals("")) {
+			rmiPort = "//:" + rmiPort + "/";
+		}
+
+		try {
+			TransactionManagerImpl obj = new TransactionManagerImpl();
+			Naming.rebind(rmiPort + TransactionManager.RMIName, obj);
+			System.out.println("TM bound");
+		} catch (Exception e) {
+			System.err.println("TM not bound:" + e);
+			System.exit(1);
+		}
 	}
 
-	try {
-	    TransactionManagerImpl obj = new TransactionManagerImpl();
-	    Naming.rebind(rmiPort + TransactionManager.RMIName, obj);
-	    System.out.println("TM bound");
-	} 
-	catch (Exception e) {
-	    System.err.println("TM not bound:" + e);
-	    System.exit(1);
+	public void ping() throws RemoteException {
 	}
-    }
-    
-    
-    public TransactionManagerImpl() throws RemoteException {
+
+	public void enlist(int xid, ResourceManager rm) throws RemoteException {
+	}
+
+	public TransactionManagerImpl() throws RemoteException {
+		this.xidCounter = 1;
+		this.dieTime = "noDie";
+		this.xids = new HashMap<>();
+		this.RMs = new HashMap<>();
+		this.xids_to_be_recovered = new HashMap<>();
+
+		
+		this.recover();
+	}
+
+	/**
+	 * recover if TM die
+	 * load data from log file, and abort  transactions not in COMMITTED state
+	*/
+	private void recover() {
+		// File dataDir = new File(DATA_ROOT);
+		// if (!dataDir.exists()) {
+		// 	dataDir.mkdirs();
+		// }
+
+		//load data from log file
+		Object xidCounterTmp = utils.loadObject(TM_COUNTER_LOG);
+		if (xidCounterTmp != null)
+			this.xidCounter = (Integer) xidCounterTmp;
+
+        Object xidsTmp = this.loadCache(TM_LOG);
+        if (xidsTmp != null) {
+            this.xids = (HashMap<Integer, String>) xidsTmp;
+        }
+
+        Object RMsTmp = this.loadCache(TM_RMs_LOG);
+        if (RMsTmp != null) {
+            this.RMs = (HashMap<Integer, HashSet<ResourceManager>>) RMsTmp;
+        }
+
+		if (xidsTmp != null) {
+			Set<Integer> xidsKeys = this.xids.keySet();
+	        for (Integer key : xidsKeys) {
+				// if a xid not in COMMITTED state
+				//it should be aborted
+				if (!this.xids.get(key).equals(TransactionManager.COMMITTED)) {
+					this.xids.put(key, TransactionManager.ABORTED);
+				}
+			}
+		}
+	}
+
+
+	
+    @Override
+    public int start() throws RemoteException {
+        synchronized (this.xidNum) {
+            Integer curXid = this.xidNum++;
+            this.storeToFile(TM_TRANSACTION_NUM_LOG_FILENAME, this.xidNum);
+
+            synchronized (this.xids) {
+                this.xids.put(curXid, TransactionManager.NEW);
+                this.storeToFile(TM_TRANSACTION_LOG_FILENAME, this.xids);
+            }
+
+            synchronized (this.xidRMs) {
+                this.xidRMs.put(curXid, new HashSet<>());
+                this.storeToFile(TM_TRANSACTION_RMs_LOG_FILENAME, this.xidRMs);
+            }
+
+            return curXid;
+        }
     }
 
-    public boolean dieNow() 
-	throws RemoteException {
-	System.exit(1);
-	return true; // We won't ever get here since we exited above;
-	             // but we still need it to please the compiler.
+
+
+    private Object loadCache(String filePath) {
+        File file = new File(filePath);
+        ObjectInputStream oin = null;
+        try {
+            oin = new ObjectInputStream(new FileInputStream(file));
+            return oin.readObject();
+        } catch (Exception e) {
+            return null;
+        } finally {
+            try {
+                if (oin != null)
+                    oin.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
     }
+
+
+
+
+	public boolean dieNow()
+			throws RemoteException {
+		System.exit(1);
+		return true; // We won't ever get here since we exited above;
+						// but we still need it to please the compiler.
+	}
 
 }
