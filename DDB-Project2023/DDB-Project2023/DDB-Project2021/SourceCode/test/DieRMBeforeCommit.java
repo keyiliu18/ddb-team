3302dc9573e23a6a2cd67ab4b23dd28db2106f57
keyi liu <1458929605@qq.com>
2024-01-05 13:30:25 +0800

    test case fix

--- DDB-Project2023/DDB-Project2023/DDB-Project2021/SourceCode/test/DieRMBeforeCommit.java
+++ DDB-Project2023/DDB-Project2023/DDB-Project2021/SourceCode/test/DieRMBeforeCommit.java
@@ -6,6 +6,7 @@ import transaction.TransactionAbortedException;
 
 
 public class DieRMBeforeCommit {
+
 	public static void main(String[] a){
 		Connector.cleanData();
 		Connector.launch("ALL");
@@ -13,7 +14,6 @@ public class DieRMBeforeCommit {
 		WorkflowController wc = Connector.connectWC();
 		 try{
 			int xid;
-
 			xid = wc.start();
 			wc.dieRMBeforeCommit("RMRooms");
 			wc.dieRMBeforeCommit("RMCars");
@@ -21,11 +21,31 @@ public class DieRMBeforeCommit {
 			wc.addCars(xid, "BYD", 1000, 879);			
 			wc.commit(xid);				
 			
-			System.out.println("DieRMBeforeCommit Test pass.");
+			Connector.launch("RMRooms");
+			Connector.launch("RMCars");
+            wc.reconnect();
+
+			xid = wc.start();
+            int ret3 = wc.queryRooms(xid, "Shanghai");
+            check(1000, ret3, "queryRooms");
+            int ret4 = wc.queryRoomsPrice(xid, "Shanghai");
+            check(999, ret4, "queryRoomsPrice");
+			int ret5 = wc.queryCars(xid, "BYD");
+            check(1000, ret5, "queryCars");
+            int ret6 = wc.queryCarsPrice(xid, "BYD");
+            check(879, ret6, "queryCarsPrice");
+
+			System.out.println("Test pass.");
 			Connector.cleanUpExit(0);
 		 }catch (Exception e) {
-            System.out.println("DieRMBeforeCommit Test fail:" + e);
+            System.out.println("Test fail:" + e);
             Connector.cleanUpExit(1);
         }
 	}
+	private static void check(int expect, int real, String method) {
+        if (expect != real) {
+            System.err.println("DieRMBeforePrepare Test fail: " + method);
+            Connector.cleanUpExit(1);
+        }
+    }
 }
--- DDB-Project2023/DDB-Project2023/DDB-Project2021/SourceCode/test/DieTMAfterCommit.java
+++ DDB-Project2023/DDB-Project2023/DDB-Project2021/SourceCode/test/DieTMAfterCommit.java
@@ -15,22 +15,44 @@ public class DieTMAfterCommit {
 			int xid;
 			   
 			xid = wc.start();               
-			wc.addFlight(xid, "B1234", 100, 500);
+			wc.addFlight(xid, "MU5377", 100, 500);
 			wc.addRooms(xid, "SHANGHAI", 300, 350);
-			wc.addCars(xid, "BEIJING", 100, 30);
-			wc.newCustomer(xid, "Mary");
+			wc.addCars(xid, "BYD", 100, 30);
 			wc.commit(xid);	
 		
 			xid = wc.start();
-			wc.addFlight(xid, "B1234", 100, 520);
+			wc.addFlight(xid, "MU5377", 100, 520);
 			wc.addRooms(xid, "SHANGHAI", 200, 300);
-			wc.addCars(xid, "BEIJING", 100, 60);
+			wc.addCars(xid, "BYD", 100, 60);
 			wc.dieTMAfterCommit();
 			try {
-               			wc.commit(xid);
-            		} catch (RemoteException e) {
-                		// e.printStackTrace();
-            		}
+				wc.commit(xid);
+			} catch (RemoteException e) {
+				// e.printStackTrace();
+			}
+			Connector.launch("TM");
+            wc.dieNow("RMFlights");
+            Connector.launch("RMFlights");
+            wc.dieNow("RMRooms");
+            Connector.launch("RMRooms");
+            wc.dieNow("RMCars");
+            Connector.launch("RMCars");
+            wc.reconnect();
+
+			xid = wc.start();
+            int r1 = wc.queryFlight(xid, "MU5377");
+            check(200, r1);
+            int r2 = wc.queryFlightPrice(xid, "MU5377");
+            check(520, r2);
+            int r3 = wc.queryRooms(xid, "SHANGHAI");
+            check(500, r3);
+            int r4 = wc.queryRoomsPrice(xid, "SHANGHAI");
+            check(300, r4);
+            int r5 = wc.queryCars(xid, "BYD");
+            check(200, r5);
+            int r6 = wc.queryCarsPrice(xid, "BYD");
+            check(60, r6);
+
 			Connector.cleanUpExit(0);
 
 		 }catch (Exception e) {
@@ -42,7 +64,7 @@ public class DieTMAfterCommit {
     private static void check(int expect, int real) {
         if (expect != real) {
             System.out.println(expect + " " + real);
-            System.err.println("DieTMAfterCommit Test fail");
+            System.err.println("Test fail");
             Connector.cleanUpExit(1);
         }
     }
